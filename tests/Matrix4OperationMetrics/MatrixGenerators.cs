8a33535b2dc7539679d323bc9b15576269675b86
Aaron Dandy <aaron.dandy@live.com>
2014-11-06 16:33:21 -0700

    a matrix inverse performance and accuracy tester

---
+++ tests/Matrix4OperationMetrics/MatrixGenerators.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vertesaur;
+
+namespace Matrix4OperationMetrics
+{
+    public static class MatrixGenerators
+    {
+
+        public static Matrix4[] GenerateMatrices(int n) {
+            var results = new Matrix4[n];
+            var rand = new Random(0);
+            for (int i = 0; i < results.Length; i++) {
+                results[i] = new Matrix4(
+                    rand.NextDouble(), rand.NextDouble(), rand.NextDouble(), rand.NextDouble(),
+                    rand.NextDouble(), rand.NextDouble(), rand.NextDouble(), rand.NextDouble(),
+                    rand.NextDouble(), rand.NextDouble(), rand.NextDouble(), rand.NextDouble(),
+                    rand.NextDouble(), rand.NextDouble(), rand.NextDouble(), rand.NextDouble()
+                );
+            }
+            return results;
+        }
+
+    }
+}
---
+++ tests/Matrix4OperationMetrics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix4OperationMetrics
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            var testData = MatrixGenerators.GenerateMatrices(100000);
+            var test = new MatrixInverseTest(testData);
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var results = test.ExecuteTests();
+            stopwatch.Stop();
+            
+            Console.WriteLine("Elapsed: " + stopwatch.Elapsed);
+            var avg = results.Average(x => x.Avg);
+            var min = results.Min(x => x.Min);
+            var max = results.Max(x => x.Max);
+
+            Console.WriteLine("Max error: " + max);
+            Console.WriteLine("Min error: " + min);
+            Console.WriteLine("Avg error: " + avg);
+
+            Console.ReadKey();
+        }
+    }
+}
