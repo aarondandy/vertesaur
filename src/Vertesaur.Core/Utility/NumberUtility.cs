5dc291e21132e219ff72bdde1a8f25cd3eab31f1
Aaron Dandy <aaron.dandy@live.com>
2014-11-06 19:02:05 -0700

    removed some dead files and added a useless swap method

---
+++ src/Vertesaur.Core/Utility/NumberUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Vertesaur.Utility
+{
+    internal static class NumberUtility
+    {
+
+        // TODO: [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Swap(ref double a, ref double b) {
+            var t = a;
+            a = b;
+            b = t;
+        }
+
+    }
+}
