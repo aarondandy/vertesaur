6a47638ac4992ae1844c5c3c95abbc005d46c11d
Aaron Dandy <aaron.dandy@live.com>
2014-11-08 17:38:12 -0700

    new build

--- src/VertesaurAssemblyInfo.cs
+++ src/VertesaurAssemblyInfo.cs
@@ -9,7 +9,7 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.12.0.*")]
+[assembly: AssemblyVersion("0.13.0.*")]
 #if DEBUG
 [assembly: AssemblyInformationalVersion("developer build")]
 #endif
